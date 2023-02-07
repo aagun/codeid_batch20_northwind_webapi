9bf185621f5f9793711e7ed91681bf59c9d2e225
kangdiancp <kangdiancp@gmail.com>
2023-02-07 12:55:04 +0700

    init project

---
+++ Northwind.Persistence/Interface/IRepositoryBase.cs
@@ -0,0 +1,28 @@
+﻿using Northwind.Persistence.RepositoryContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Persistence.Contract
+{
+    public interface IRepositoryBase<T>
+    {
+        IEnumerator<T> FindAll<T>(string sql);
+
+        IEnumerator<T> FindByCondition<T>(SqlCommandModel model);
+
+        IAsyncEnumerator<T> FindAllAsync<T>(SqlCommandModel model);
+
+        void Create(SqlCommandModel model);
+
+        void Update(SqlCommandModel model);
+
+        void Delete(SqlCommandModel model);
+
+  
+
+    }
+}
